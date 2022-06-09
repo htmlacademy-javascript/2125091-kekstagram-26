1315d9402a066f5a02e5f4bc4a4d8afb565ec8f5
BalSerg <motopes@yandex.ru>
2022-06-09 23:01:57 +0300

    Fix review

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 function getNumberFromInterval(startInterval, endInterval) {
-  if((endInterval > startInterval) && (startInterval !== 0) && (endInterval !== 0)) {
-    console.log(Math.floor(Math.random() * (endInterval - startInterval + 1) + startInterval));
+  if(endInterval !== startInterval) {
+    return Math.floor(Math.random() * (endInterval - startInterval + 1) + startInterval);
   }
+
+  return endInterval;
 }
 
-getNumberFromInterval(7, 17);
+getNumberFromInterval(0, 17);
 
 
 function checkLengthString(yourString, lengthString){
-  yourString = String(yourString);
-  lengthString = +lengthString;
-
   if(yourString.length >= lengthString) {
     return false;
   }
