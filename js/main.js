2aab58889c6e03e12355be60af7e1f5618b1ed12
BalSerg <motopes@yandex.ru>
2022-06-09 17:31:21 +0300

    Done task2

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function getNumberFromInterval(startInterval, endInterval) {
+  if((endInterval > startInterval) && (startInterval !== 0) && (endInterval !== 0)) {
+    console.log(Math.floor(Math.random() * (endInterval - startInterval + 1) + startInterval));
+  }
+}
+
+getNumberFromInterval(7, 17);
+
+
+function checkLengthString(yourString, lengthString){
+  yourString = String(yourString);
+  lengthString = +lengthString;
+
+  if(yourString.length >= lengthString) {
+    return false;
+  }
+
+  return true;
+}
+
+checkLengthString('Nord Wing has not An-2', 6);
